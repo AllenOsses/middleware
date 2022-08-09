bf64cf6de02717c651861ea3d1f7f6aa27c04fc8
Allen <allenn83@gmail.com>
2022-08-09 18:40:44 -0400

    filtrado en rutas con metodo auth

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{ /*
+    //por lo general se protege la ruta,asi el controlador no sera largo
+   public function __construct(){
+   $this->middleware('auth');
+   }*/
+}
